2a26776f237603dc77a671c3338ec120e9cc7da3
ezzyjuliansyah <ezzyjuliansyah1@gmail.com>
2025-10-02 10:01:38 +0700

    pesan commit

---
+++ jobsheet5/ifCetakKRS09.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ifCetakKRS09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("---Cetak KRS Siakad---");
+        System.out.print("Apakah UKT sudah lunas? (true/false):");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas);
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silakan Cetak KRS dan minta tanda tangan DPA");
+            
+    }
+}
